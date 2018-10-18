9f16f286f5f97847776957647778abf63f31655d
Diego Diez <diego10ruiz@gmail.com>
2018-10-18 15:03:15 +0900

    Change arguments (pass down ...) for theme_base().

--- R/plot.R
+++ R/plot.R
@@ -3,9 +3,12 @@
 #' A tweaked ggthemes::theme_base().
 #'
 #' @param base_size base size for text.
-#' @param base_family base family for text.
+#' @param ... arguments passed down to theme_dark().
 #'
 #' @export
-theme_base <- function(base_size = 12, base_family = "") {
-  ggthemes::theme_base(base_size = base_size, base_family = base_family) + theme(plot.background = element_rect(color = NA))
+theme_base <- function(base_size = 12,...) {
+  ggthemes::theme_base(base_size = base_size, ...) +
+    theme(plot.background = element_rect(color = NA))
+}
+
 }
