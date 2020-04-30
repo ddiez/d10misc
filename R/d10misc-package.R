4e8da129d8e19f609a7aa727be8dcbb79b2763b4
Diego Diez <diego10ruiz@gmail.com>
2020-04-30 11:53:20 +0900

    Add package file.

---
+++ R/d10misc-package.R
@@ -0,0 +1,12 @@
+#' @keywords internal
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+#' @import tibble dplyr tidyr ggplot2
+#' @importFrom ggthemes theme_base
+#' @importFrom rlang := !!
+#' @importFrom stats as.dist dist hclust cor
+## usethis namespace: end
+NULL
